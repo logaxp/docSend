926d72a06d1593b8ee757d67b2621a53c683a2ef
Judisoft <kumjude09@gmail.com>
2024-01-06 13:44:38 +0100

    get email from GET request header

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(express.json({ limit: '500mb' }));
 
 app.get('/editor', (req, res) => {
-    res.render('index.ejs');
+    res.render('editor-ui.ejs');
 });
 
 
