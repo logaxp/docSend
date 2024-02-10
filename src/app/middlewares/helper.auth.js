fee8cfabef07a449a3ec3adb0f4fdd0de4821591
Judisoft <kumjude09@gmail.com>
2024-02-10 22:20:13 +0100

    helper.auth modified

--- src/app/middlewares/helper.auth.js
+++ src/app/middlewares/helper.auth.js
@@ -2,41 +2,47 @@ const Jwt = require('jsonwebtoken')
 const { StatusCodes } = require('http-status-codes')
 const dotenv = require('dotenv')
 
+dotenv.config()
 
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.headers.authorization
 
-const authMiddleware = async (req, res, next) => {
-    // check header
-    const authHeader = req.headers.authorization
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      msg: 'Unauthorized request header format',
+      status: StatusCodes.BAD_REQUEST
+    })
+  }
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            msg: 'Unauthorized request header format',
-            status: StatusCodes.BAD_REQUEST
-        })
+  const token = authHeader.split(' ')[1]
+
+  if (!token) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      msg: 'Token not provided',
+      status: StatusCodes.BAD_REQUEST
+    })
+  }
+
+  try {
+    const decodedToken = Jwt.verify(token, process.env.SECRET_KEY)
+    req.user = {
+      authId: decodedToken.authId,
+      email: decodedToken.email
     }
-
-    const token = authHeader.split(' ')[1]
-
-    if (!token) {
-        return res.status(StatusCodes.BAD_REQUEST).json({
-            msg: 'Token not provided',
-            status: StatusCodes.BAD_REQUEST
-        })
+    next()
+  } catch (err) {
+    console.log(err)
+    if (err.name === 'TokenExpiredError') {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        msg: 'Token has expired',
+        status: StatusCodes.UNAUTHORIZED
+      })
     }
-
-    dotenv.config();
-    try {
-        const response = Jwt.verify(token, process.env.SECRET_KEY)
-        console.log(response);
-        req.user = { authId: response.authId, email: response.email }
-        next()
-    } catch (err) {
-        // Handle specific error types if needed
-        return res.status(StatusCodes.UNAUTHORIZED).json({
-            msg: 'Invalid access token',
-            status: StatusCodes.UNAUTHORIZED
-        })
-    }
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      msg: err.message,
+      status: StatusCodes.UNAUTHORIZED
+    })
+  }
 }
 
 module.exports = authMiddleware
